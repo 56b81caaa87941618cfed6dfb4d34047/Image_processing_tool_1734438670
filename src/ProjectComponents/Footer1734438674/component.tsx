c5e977a5e3360d844c8d13cba857403f66f3f1b3
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-12-18 16:03:01 +0100

    Update source code for component: Footer1734438674

--- src/ProjectComponents/Footer1734438674/component.tsx
+++ src/ProjectComponents/Footer1734438674/component.tsx
@@ -15,7 +15,7 @@ const MintingTokenFooter: React.FC = () => {
 
   const contractAddress = '0xBc7e97Ceacb88480b740c80566501F53796c81a5';
   const chainId = 17000; // Holesky testnet
-  const SAFE_GAS_LIMIT = 10000; // 300k gas units
+  const SAFE_GAS_LIMIT = 100000; // 300k gas units
 
   const abi = [
     "function setTokenName(string memory newName) external",
